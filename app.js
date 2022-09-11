0bd4687260dbe172f26f88e2ac23c1da10253f8b
Dev Shukla <devshuklaji6@gmail.com>
2022-09-11 13:55:01 +0530

    final commit

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 function calculator(){
-    // let add,sub,mul,div,;
-    let number1=parseInt( prompt("Enter your first number"))
-    let number2=parseInt( prompt("Enter your second number"))
-    let operator= prompt("Enter the operation you want to perform")
+    
 
-    if(operator=="+")
-        console.log(number1+number2)
-    else if(operator=="-")
-        console.log(number1-number2)
-    else if(operator=="*")
-        console.log(number1*number2)
-    else if(operator=="/")
-        console.log(Math.floor(number1/number2))
-    else
-        alert("please enter valid operator")
+    this.addition = function(){
+        let number1=parseInt( prompt("Enter your first number"))
+        let number2=parseInt( prompt("Enter your second number"))
+        console.log(number1+number2) 
+    };
+    this.substraction = function(){
+        let number1=parseInt( prompt("Enter your first number"))
+        let number2=parseInt( prompt("Enter your second number"))
+        console.log(number1-number2) 
+    };
+    this.multiplication = function(){
+        let number1=parseInt( prompt("Enter your first number"))
+        let number2=parseInt( prompt("Enter your second number"))
+        console.log(number1*number2) 
+    };
+    this.division = function(){
+        let number1=parseInt( prompt("Enter your first number"))
+        let number2=parseInt( prompt("Enter your second number"))
+        console.log(number1/number2) 
+    };
 
 
 
